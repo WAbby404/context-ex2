59401d5274c3af95da426516bdf26e28bae27ad7
Abby Waddell <wabby.404@gmail.com>
2024-06-26 15:50:09 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import ComponentA from "./ComponentA";
+import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <ComponentA />;
 }
 
 export default App;
+
+// userContext() = React Hook that allows you to share values
+//                 between multiple levels of components
+//                 without passing props through each level
+
+// Provider Component
+// which component has the data we need access to? (App)
+// 1. import {createContext} from 'react';
+// 2. export const MyContext = createContext();
+// 3. <MyContext.Provider value={value}>
+//      <Child />
+//    </MyContext.Provider>
+// Component A is now the provider component, provides value of user
+
+// Consumer Components
+// 1. import React, {useContext} from 'react';
+//    import {MyContext} from './ComponentA';
+// 2. const value = useContext(MyContext);
+// UseContext will traverse up the component tree to find the nearest provider (from ComponentD to ComponentA)
+
+// cannot have more than 1
